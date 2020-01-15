2ee9192fb9d52b9dd49e4b8c100f10299d27c93d
Meghanath91 <meghanath.balaji@gmail.com>
2020-01-15 19:05:56 +0000

    play and client updated

--- client.js
+++ client.js
@@ -1,4 +1,35 @@
-// play.js
-const { connect } = require('./client');
-console.log('Connecting ...');
-connect();
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.2.204',
+    port: 50541
+    
+  });
+
+  //
+  conn.on('connect', () => {
+    console.log('connected successfully');
+    conn.write('Name: MEG');
+    conn.write('Move: up');
+    // conn.write('Move: down ');
+    
+  });
+  
+
+  // client.js
+  conn.on('data', (data) => {
+  console.log('Server says: ', data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+};
+
+module.exports = {
+  connect };
